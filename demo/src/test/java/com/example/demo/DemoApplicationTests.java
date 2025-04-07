d0b65be4ee265dabba2811f432c7db9b8473148b
TurboBoyarin <timosha.astashov@mail.ru>
2025-04-08 01:46:55 +0300

    Delete_metod_test

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -25,25 +25,25 @@ public class DemoApplicationTests {
 		// Здесь можно выполнить настройку перед каждым тестом, если это необходимо
 	}
 
-	@Test
-	@WithMockUser (username = "admin", roles = {"ADMIN"})
-	public void testCreateComandWithAdminRole() throws Exception {
-		mockMvc.perform(get("/comands/create"))
-				.andExpect(status().isOk())
-				.andExpect(view().name("createComand")); // Убедитесь, что возвращается правильное представление
-	}
+	// @Test
+	// @WithMockUser (username = "admin", roles = {"ADMIN"})
+	// public void testCreateComandWithAdminRole() throws Exception {
+	// 	mockMvc.perform(get("/comands/create"))
+	// 			.andExpect(status().isOk())
+	// 			.andExpect(view().name("createComand")); // Убедитесь, что возвращается правильное представление
+	// }
 
-	@Test
-	@WithMockUser (username = "user", roles = {"USER"})
-	public void testCreateComandWithUserRole() throws Exception {
-		mockMvc.perform(get("/comands/create"))
-				.andExpect(status().isForbidden()); // Доступ запрещен для пользователей без роли ADMIN
-	}
+	// @Test
+	// @WithMockUser (username = "user", roles = {"USER"})
+	// public void testCreateComandWithUserRole() throws Exception {
+	// 	mockMvc.perform(get("/comands/create"))
+	// 			.andExpect(status().isForbidden()); // Доступ запрещен для пользователей без роли ADMIN
+	// }
 
-	@Test
-	public void testCreateComandWithoutAuthentication() throws Exception {
-		mockMvc.perform(get("/comands/create"))
-				.andExpect(status().is3xxRedirection()) // Ожидаем перенаправление на страницу входа
-				.andExpect(redirectedUrl("/login")); // Убедитесь, что перенаправление на страницу входа
-	}
+	// @Test
+	// public void testCreateComandWithoutAuthentication() throws Exception {
+	// 	mockMvc.perform(get("/comands/create"))
+	// 			.andExpect(status().is3xxRedirection()) // Ожидаем перенаправление на страницу входа
+	// 			.andExpect(redirectedUrl("/login")); // Убедитесь, что перенаправление на страницу входа
+	// }
 }
